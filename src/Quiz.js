17430cfe5263272c88b47013a4317e3daecf4995
Gabriele Bianchet-David <gbianchetdavid@ebsco.com>
2018-10-13 12:55:49 -0400

    Finished module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,10 @@ class Quiz extends Component {
       quiz_position: 1
     };
   }
+
+  showNextQuestion() {
+    this.setState({ quiz_position: this.state.quiz_position + 1});
+  }
   
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -20,7 +24,9 @@ class Quiz extends Component {
         {isQuizEnd ? (
           <QuizEnd />
         ) : (
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+          <QuizQuestion
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
+            quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
         )}
       </div>
     );
